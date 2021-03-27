38d2bf47c2f3973fd7914141d549adfba9a57196
naydennn <na44kata@gmail.com>
2021-03-27 17:49:37 +0200

    add exercises.

--- Java-OOP/UnitTesting-Exercises/UnitTesting-Exercises/src/main/java/ExtendedDatabase/Database.java
+++ Java-OOP/UnitTesting-Exercises/UnitTesting-Exercises/src/main/java/ExtendedDatabase/Database.java
@@ -1,7 +1,6 @@
 package ExtendedDatabase;
 
 import javax.naming.OperationNotSupportedException;
-import java.util.Arrays;
 
 public class Database {
 
@@ -9,15 +8,17 @@ public class Database {
     private int index;
 
     public Database(Person... people) throws OperationNotSupportedException {
-        this.people = new Person[16];
         this.index = 0;
         setPerson(people);
     }
 
     public void setPerson(Person[] people) throws OperationNotSupportedException {
+        if (people.length > 16 || people.length < 1) {
+            throw new OperationNotSupportedException();
+        }
+        this.people = new Person[16];
         for (Person person : people) {
             addPerson(people[index]);
-            this.index++;
         }
     }
 
@@ -26,13 +27,20 @@ public class Database {
         boolean isExist = false;
 
         try {
-            if (person.getId() == null || person.getId() < 0) {
-                isExist = Arrays.stream(this.people).anyMatch(p -> p.getId().equals(person.getId()));
-                if (isExist) {
-                    throw new OperationNotSupportedException();
+            if (person.getId() == null || person.getId() < 0 || person.getName() == null) {
+                throw new OperationNotSupportedException();
+            }
+            for (Person p : this.people) {
+                if (p != null) {
+                    isExist = p.getId().equals(person.getId());
+                    break;
                 }
             }
+            if (isExist) {
+                throw new OperationNotSupportedException();
+            }
             this.people[this.index] = person;
+            this.index++;
         } catch (NullPointerException ignored) {
             throw new OperationNotSupportedException();
         }
@@ -40,15 +48,16 @@ public class Database {
     }
 
     public void remove() throws OperationNotSupportedException {
-        if (this.people[index] == null) {
+        try {
+            this.people[index - 1] = null;
+            index--;
+        } catch (ArrayIndexOutOfBoundsException ignored) {
             throw new OperationNotSupportedException();
         }
-        this.people[index] = null;
-        index--;
     }
 
     public Person[] getPeople() {
-        Person[] bufferPeople = new Person[index + 1];
+        Person[] bufferPeople = new Person[index];
 
         int bufferIndex = 0;
         for (Person person : this.people) {
@@ -58,5 +67,38 @@ public class Database {
             }
         }
         return bufferPeople;
+    }
+
+    public Person findById(Long id) throws OperationNotSupportedException {
+
+        Person person = null;
+
+        for (Person p : this.people) {
+            if (p != null) {
+                if (p.getId().equals(id)) {
+                    person = p;
+                }
+            }
+        }
+        if (person == null) {
+            throw new OperationNotSupportedException();
+        }
+        return person;
+    }
+
+    public Person findByName(String name) throws OperationNotSupportedException {
+        Person person = null;
+
+        for (Person p : this.people) {
+            if (p != null) {
+                if (p.getName().equals(name)) {
+                    person = p;
+                }
+            }
+        }
+        if (name == null || name.trim().isEmpty() || person == null) {
+            throw new OperationNotSupportedException();
+        }
+        return person;
     }
 }
--- Java-OOP/UnitTesting-Exercises/UnitTesting-Exercises/src/test/java/ExtendedDatabase/DatabaseTest.java
+++ Java-OOP/UnitTesting-Exercises/UnitTesting-Exercises/src/test/java/ExtendedDatabase/DatabaseTest.java
@@ -46,4 +46,68 @@ public class DatabaseTest {
     public void testAddPeopleWithExistingId() throws OperationNotSupportedException {
         this.database.addPerson(new Person("PersonWithExistId" , 19L));
     }
+    //remove
+
+    @Test
+    public void testRemovePersonFromDatabaseCorrectly() throws OperationNotSupportedException {
+        this.database.remove();
+
+        Person[] people = this.database.getPeople();
+        Person actualPerson = people[people.length - 1];
+
+        assertEquals(actualPerson.getName(), "Peter");
+    }
+
+    @Test(expected = OperationNotSupportedException.class)
+    public void testRemoveFromEmptyDatabase() throws OperationNotSupportedException {
+        for (int i = 0; i < 3; i++) {
+            this.database.remove();
+        }
+    }
+    //constructor
+
+    @Test(expected = OperationNotSupportedException.class)
+    public void addMorePeopleThanCapacity() throws OperationNotSupportedException {
+        Person[] arr = new Person[17];
+        new Database(arr);
+    }
+
+    @Test(expected = OperationNotSupportedException.class)
+    public void testAddNull() throws OperationNotSupportedException {
+        this.database.addPerson(null);
+    }
+
+    //find by ID
+
+    @Test(expected = OperationNotSupportedException.class)
+    public void testSearchWithInvalidId() throws OperationNotSupportedException {
+        this.database.findById(17L);
+    }
+
+    @Test
+    public void testFindPeopleById() throws OperationNotSupportedException {
+        Person personById = this.database.findById(19L);
+
+        assertEquals("Peter",personById.getName());
+    }
+
+    // find person by name
+
+    @Test
+    public void testFindPeopleByRealName() throws OperationNotSupportedException {
+        Person peter = this.database.findByName("Peter");
+
+        Long expectedId = 19L;
+        assertEquals(expectedId , peter.getId());
+    }
+
+    @Test(expected = OperationNotSupportedException.class)
+    public void testIfPersonNameIsEmpty() throws OperationNotSupportedException {
+        this.database.findByName(" ");
+    }
+
+    @Test(expected = OperationNotSupportedException.class)
+    public void testIfPersonNameIsNull() throws OperationNotSupportedException {
+        this.database.findByName(null);
+    }
 }
