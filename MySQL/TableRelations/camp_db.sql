b391c363cbaf2aad675391395aba8bce7595e332
naydennn <na44kata@gmail.com>
2021-06-07 12:12:44 +0300

    add exercises.

--- MySQL/TableRelations/camp_db.sql
+++ MySQL/TableRelations/camp_db.sql
@@ -88,3 +88,36 @@ CREATE TABLE peaks (
     CONSTRAINT fk_peaks_mountains FOREIGN KEY (mountain_id)
         REFERENCES mountains (id)
 );
+
+SELECT 
+    driver_id,
+    vehicle_type,
+    CONCAT_WS(' ', first_name, last_name) AS driver_name
+FROM
+    vehicles
+        JOIN
+    campers ON vehicles.driver_id = campers.id;
+
+SELECT 
+    starting_point,
+    end_point,
+    leader_id,
+    CONCAT(first_name, ' ', last_name) AS leader_name
+FROM
+    routes
+        JOIN
+    campers ON routes.leader_id = campers.id;
+
+CREATE TABLE mountains (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(50)
+);
+
+CREATE TABLE peaks (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(50),
+    mountain_id INT,
+    CONSTRAINT fk_peaks_mountains FOREIGN KEY (mountain_id)
+        REFERENCES mountains (id)
+        ON DELETE CASCADE
+);
