95e7e8d347594350b4d36daf04dff68c6db3361c
naydennn <na44kata@gmail.com>
2021-06-08 22:31:49 +0300

    add exercises.

--- MySQL/TableRelations/geography_database.sql
+++ MySQL/TableRelations/geography_database.sql
@@ -857,3 +857,174 @@ VALUES
 (104, 'Model X', 2),
 (105, 'Model 3', 2),
 (106, 'Nova', 3);
+
+CREATE TABLE students (
+student_id INT PRIMARY KEY AUTO_INCREMENT,
+`name` VARCHAR(45)
+);
+
+CREATE TABLE exams (
+exam_id INT PRIMARY KEY AUTO_INCREMENT,
+`name` VARCHAR(45)
+);
+
+CREATE TABLE students_exams (
+student_id INT,
+exam_id INT,
+CONSTRAINT pk_students_exams
+PRIMARY KEY(student_id, exam_id),
+CONSTRAINT fk_students_exams_students
+FOREIGN KEY (student_id)
+REFERENCES students(student_id),
+CONSTRAINT fk_students_exams_exams
+FOREIGN KEY (exam_id)
+REFERENCES exams(exam_id)
+);
+
+INSERT INTO students (`name`)
+VALUES
+('Mila'),
+('Toni'),
+('Ron');
+
+INSERT INTO exams
+VALUES
+(101, 'Spring MVC'),
+(102, 'Neo4j'),
+(103, 'Oracle 11g');
+
+INSERT INTO students_exams
+VALUES
+(1,101),
+(1,102),
+(2,101),
+(3,103),
+(2,102),
+(2,103);
+
+CREATE DATABASE online_store;
+USE online_store;
+
+CREATE TABLE item_types (
+item_type_id INT PRIMARY KEY AUTO_INCREMENT,
+`name` VARCHAR(50)
+);
+
+CREATE TABLE items (
+item_id INT PRIMARY KEY AUTO_INCREMENT,
+`name` VARCHAR(50),
+item_type_id INT,
+CONSTRAINT fk_items_item_types
+FOREIGN KEY (item_type_id)
+REFERENCES item_types(item_type_id)
+);
+
+CREATE TABLE cities (
+city_id INT PRIMARY KEY AUTO_INCREMENT,
+`name` VARCHAR(50)
+);
+
+CREATE TABLE customers (
+customer_id INT PRIMARY KEY AUTO_INCREMENT,
+`name` VARCHAR (50),
+birthday DATE,
+city_id INT,
+CONSTRAINT fk_customers_cities
+FOREIGN KEY (city_id)
+REFERENCES cities(city_id)
+);
+
+CREATE TABLE orders (
+order_id INT PRIMARY KEY AUTO_INCREMENT,
+customer_id INT,
+CONSTRAINT fk_orders_customers
+FOREIGN KEY (customer_id)
+REFERENCES customers(customer_id)
+);
+
+CREATE TABLE order_items (
+    order_id INT,
+    item_id INT,
+    CONSTRAINT pk_orders_items PRIMARY KEY (order_id , item_id),
+    CONSTRAINT fk_orders_items_orders FOREIGN KEY (order_id)
+        REFERENCES orders (order_id),
+    CONSTRAINT fk_orders_items_items FOREIGN KEY (item_id)
+        REFERENCES items (item_id)
+);
+
+CREATE TABLE teachers (
+teacher_id INT PRIMARY KEY AUTO_INCREMENT,
+`name` VARCHAR(45),
+manager_id INT
+);
+
+ALTER TABLE teachers AUTO_INCREMENT = 101;
+
+INSERT INTO teachers(`name`, manager_id)
+VALUES
+('John', NULL),
+('Maya', 106),
+('Silvia', 106),
+('Ted', 105),
+('Mark', 101),
+('Greta', 101);
+
+ALTER TABLE teachers
+ADD CONSTRAINT fk_teachers_teachers
+FOREIGN KEY (manager_id)
+REFERENCES teachers(teacher_id);
+
+CREATE DATABASE university;
+USE university;
+
+CREATE TABLE majors (
+major_id INT(11) PRIMARY KEY AUTO_INCREMENT,
+`name` VARCHAR(50)
+);
+
+CREATE TABLE students (
+student_id INT PRIMARY KEY AUTO_INCREMENT,
+student_number VARCHAR(12),
+student_name VARCHAR(50),
+major_id INT,
+CONSTRAINT fk_students_majors
+FOREIGN KEY (major_id)
+REFERENCES majors(major_id)
+);
+
+CREATE TABLE payments (
+payment_id INT PRIMARY KEY AUTO_INCREMENT,
+payment_date DATE,
+payment_amount DECIMAL(8,2),
+student_id INT,
+CONSTRAINT fk_payments_students
+FOREIGN KEY (student_id)
+REFERENCES students(student_id)
+);
+
+CREATE TABLE subjects (
+subject_id INT PRIMARY KEY AUTO_INCREMENT,
+subject_name VARCHAR(50)
+);
+
+CREATE TABLE agenda(
+student_id INT,
+subject_id INT,
+CONSTRAINT pk_students_subjects
+PRIMARY KEY (student_id, subject_id),
+CONSTRAINT fk_agenda_students
+FOREIGN KEY (student_id)
+REFERENCES students(student_id),
+CONSTRAINT fk_agenda_subjects
+FOREIGN KEY (subject_id)
+REFERENCES subjects(subject_id)
+);
+
+USE geography;
+
+SELECT m.mountain_range, p.peak_name, p.elevation
+FROM mountains AS m
+JOIN peaks AS p
+ON p.mountain_id = m.id 
+WHERE m.mountain_range LIKE 'Rila' 
+ORDER BY elevation DESC;
